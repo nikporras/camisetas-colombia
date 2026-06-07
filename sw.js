fd1a66ea24a032b9bc2b39275cc97b74a276d534
Nico <porras456@hotmail.com>
2026-06-07 18:49:22 +0100

    Agregar tab Gastos + fix ingresos + bump cache v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='cam-v2';
+const C='cam-v3';
 const A=['./index.html','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim();});
